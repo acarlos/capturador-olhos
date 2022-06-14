dc2a8cd82c27c41b1b7f906ce8acb96aa76d34ad
acarlos <acarlos@nicts>
2022-06-14 15:09:43 -0300

    em branco

--- src/main/java/gui/RastreadorOlhos.java
+++ src/main/java/gui/RastreadorOlhos.java
@@ -6,7 +6,7 @@ public class RastreadorOlhos {
 
 	/**
 	 * @param args
-	 *            the command line arguments
+	 *            the command line arguments 
 	 */
 	public static void main(String args[]) {
 
